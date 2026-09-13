94c5bf40a9e9c30496ddec63831c7e028b5c4781
Sofia Rodrigues Rocha <sofia_srr@outlook.com.br>
2026-09-13 00:37:15 -0300

     refactor(funcionario): ajusta responsabilidade do metodo novo na action

--- src/main/java/br/com/soc/sistema/action/FuncionarioAction.java
+++ src/main/java/br/com/soc/sistema/action/FuncionarioAction.java
@@ -33,12 +33,7 @@ public class FuncionarioAction extends Action {
 	}
 	
 	public String novo() {
-		if(funcionarioVo.getNome() == null)
-			return INPUT;
-		
-		business.salvarFuncionario(funcionarioVo);
-		
-		return REDIRECT;
+	    return INPUT;
 	}
 	
 	public String editar() {
